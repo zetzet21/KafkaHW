56c90a6b01779337c2fd340157dc3041e1884c3d
zetzet21 <ilyabeskrovny@mail.ru>
2024-03-24 22:43:14 +0400

    first commit

---
+++ src/main/java/com/t1/kafkahw/KafkaHwApplication.java
@@ -0,0 +1,13 @@
+package com.t1.kafkahw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/t1/kafkahw/KafkaHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1.kafkahw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
